a62055f9c783f5a2a7fdc53bce1d03b4c7e2b87d
Anadi Chiransa <anadipiyamini@gmail.com>
2026-01-02 23:12:25 +0530

    Added second test case

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,8 +9,8 @@ describe ("Maison function tests" , () => {
         render(<App/>);
     });
 
-    //Search Filtering by text
-    it("Filters properties correctly when user typed the location", () => {
+    //Test - 01: Search Filtering by text
+    it("Filters properties correctly when typed the location", () => {
         const searchInput = screen.getByPlaceholderText(/e. g. Colombo/i);
         fireEvent.change(searchInput, { target: {value: "Colombo"} });
 
@@ -18,6 +18,21 @@ describe ("Maison function tests" , () => {
         const propertyCards = screen.queryAllByRole("heading", {level:3});
         propertyCards.forEach(card => {
             expect(card.textContent).not.toMatch(/Kandy/i);
+        });
+    });
+
+    //Test - 02: Price Filtering 
+    it("Filters properties based on entered minimum price input", () => {
+        const minPriceInput = screen.getByPlaceholderText(/Min Price/i);
+
+        //Setting high min price which will hide cheap properties
+        fireEvent.change(minPriceInput, { target: {value: "100000000" } });
+
+        //Check whether properties below 100M are hidden
+        const priceElements = screen.queryAllByText(/\$/);
+        priceElements.forEach(price => {
+            const numericPrice = parseInt(price.textContent.replace(/[^0-9]/g, ''));
+            expect(numericPrice).toBeGreaterThanOrEqual(100000000);
         });
     });
 
