4480a2f93426e275dc2ba5127e0f260ccf98e3a2
Anadi Chiransa <anadipiyamini@gmail.com>
2026-01-02 15:03:19 +0530

    Added thumbanials for the extra images

--- src/components/PropertyPage.jsx
+++ src/components/PropertyPage.jsx
@@ -1,10 +1,13 @@
-import {useParams, useNavigate} from "react-router-dom";
 import React from "react";
+import {useParams, useNavigate} from "react-router-dom";
 
 
 const PropertyPage = ({properties}) => {
     const {id} = useParams();
     const navigate = useNavigate();
+
+    //Sate for tabs
+    const [activeTab, setActiveTab] = useState("description");
 
     //Finding the property using the Id from the URL 
     const property = properties.find(p => p.id === id);
@@ -15,7 +18,25 @@ const PropertyPage = ({properties}) => {
         <div className="property-page">
             <button className="back-btn" onClick={() => navigate(-1)}> ← Back To Search </button>
             <h1> {property.type} in {property.location} </h1>
-            <img src={`/${property.picture}`} alt= {property.type} className="detail-main-image"/>
+
+            {/*main gallert section*/}
+            <div className="gallery-container">
+                <div className="main-image-view">
+                    <img src={`/${property.picture}`} alt= {property.type} className="detail-main-image"/>
+                </div>
+
+                {/*Thumbanails for extra images*/}
+                <div className="thumbnail-grid">
+                    {property.images && property.images.map((img,index) =>(
+                        <img key = {index}
+                            src ={`/${img}`}
+                            alt={`view${index}`}
+                            className="thumbnail"
+                            />
+                    ))}
+                </div>
+            </div>
+            
             <p className="detail-price"> Price: ${property.price.toLocaleString()}</p>
         </div>
     );
