f54614b7ed47b9bd981985adf70e732a37eb9cf8
Anadi Chiransa <anadipiyamini@gmail.com>
2026-01-03 12:08:09 +0530

    Added json list to use in test file

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,6 +3,21 @@ import { describe, it, expect, beforeEach} from "vitest";
 import App from "./App";
 import "@testing-library/jest-dom";
 
+describe("Maison Function Testing" , () => {
+
+    //block "mocks" the fetch call to avoid the URL error
+    global.fetch = vi.fn() =>
+        Promise.resolve({
+            json:() => Promise.resolve({
+                properties: [
+                    { id: "prop1", type: "House", price: 85000000, location:"Colombo", bedrooms: 4, added: {month:"Jan",day:1,year:2026} },
+                    { id: "prop2", type:"Flat",price:35000000,location: "Kandy", bedrooms:2, added: {month:"Jan", day:1, year:2026}}
+                ]
+            }),
+        })
+    );
+
+    
 
 
     
